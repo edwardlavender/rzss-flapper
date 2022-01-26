864843810a7ebeb4f8a919ee3113ded2970d499c
Edward Lavender <el72@st-andrews.ac.uk>
2022-01-26 10:53:59 +0000

    Define core list of response variables and associoated labels in define_global_param.R.

--- R/define_global_param.R
+++ R/define_global_param.R
@@ -31,6 +31,31 @@ library(prettyGraphics)
 contains_qu        <- function(x) stringi::stri_detect_fixed(x, "?")
 contains_less_than <- function(x) stringi::stri_detect_fixed(x, "<")
 
+#### Response variables
+## Define core list of response variables 
+# Exclude: BE, SO2, Ca, Mg (?)
+resps <- c("pH", "PCO2", "PO2", "HCO3", "lac", "glu", "K", "Mg")
+## Define pretty axis titles
+ylabs <- list(pH = "pH", 
+              PCO2 = expression(PCO[2] ~ "[mmHg]"), 
+              PO2 = expression(PO[2] ~ "[mmHg]"), 
+              HCO3 = expression(HCO[3] ~ "[mmol" * L^-1 * "]"),
+              lac = expression("Lactate [mmol" * L^-1 * "]"), 
+              glu = expression("Glucose [mmol" * L^-1 * "]"), 
+              K = expression("K [mmol" * L^-1 * "]"), 
+              Mg = expression("Mg [mmol" * L^-1 * "]")
+              )
+# Check titles
+ylabs_check <- FALSE
+if(ylabs_check){
+  pp <- par(mfrow = par_mf(length(resps)))
+  for(i in 1:length(resps)){
+    plot(0, main = ylabs[i])
+  }
+  par(pp)
+}
+
+
 
 #### End of code. 
 ################################
