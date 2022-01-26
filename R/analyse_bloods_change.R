5934f765cc3b71dc963e88a8a0858487db9da200
Edward Lavender <el72@st-andrews.ac.uk>
2022-01-26 10:58:29 +0000

    Update analyse_bloods_change with core list of response variables and tidy variable names.

--- R/analyse_bloods_change.R
+++ R/analyse_bloods_change.R
@@ -20,16 +20,18 @@ source("./R/define_global_param.R")
 #### Load data
 physio <- readRDS("./data/physio.rds")
 
+#### Define local parameters
+# Define whether or not to save figures
+save <- TRUE
+
+
 ################################
 ################################
 #### Plot BS1 and BS2 
 
-#### Define response variables
-resps <- c("pH", "pCO2", "pO2", "be", "HCO3", "SO2", "lac", "glu", "K", "Ca", "Mg")
-
 #### Loop over each response and make plots
-png("./fig/blood_sample_change.png", 
-    height = 6, width = 8, units = "in", res = 800)
+if(save) tiff("./fig/blood_sample_change.tiff", 
+              height = 6, width = 8, units = "in", res = 800)
 pp <- par(mfrow = par_mf(length(resps)), 
           oma = c(4, 4, 2, 2), mar = c(2, 2, 2, 2))
 prompt <- FALSE # TRUE
@@ -37,6 +39,7 @@ lapply(1:length(resps), function(i){
   
   #### Define response
   resp <- resps[i]
+  ylab <- ylabs[[resp]]
   resp_1 <- paste0(resp, "_1")
   resp_2 <- paste0(resp, "_2")
   y          <- c(physio[, resp_1], physio[, resp_2])
@@ -51,16 +54,16 @@ lapply(1:length(resps), function(i){
   #### Make boxplot
   pretty_boxplot(x, y, 
                  varwidth = TRUE,
-                 ylab = resp,
+                 ylab = "",
                  pretty_axis_args = list(side = 1:2, control_digits = 1)
                  )
-  mtext(side = 2, resp, line = 2)
+  mtext(side = 2, ylab, line = 2)
   mtext(side = 3, LETTERS[i], font = 2, adj = 0)
   if(prompt) readline("Press [Enter] to continue...")
 }) %>% invisible()
 mtext(side = 1, "Blood sample", line = 1, outer = TRUE)
 par(pp)
-dev.off()
+if(save) dev.off()
 
 #### End of code. 
 ################################
