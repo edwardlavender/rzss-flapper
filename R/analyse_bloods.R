ad5f57522089bc999153a433a9f796d1319e82a1
Edward Lavender <el72@st-andrews.ac.uk>
2022-02-18 13:36:40 +0000

    Permit temperature-dependent effects of fight time in models of blood parameter values and reorder terms.

--- R/analyse_bloods.R
+++ R/analyse_bloods.R
@@ -1,6 +1,6 @@
 ################################
 ################################
-#### analyse_bloods.R
+#### analyse_bloods_synthesis.R
 
 #### This script: 
 # 1) Analyses skate blood parameters. 
@@ -44,8 +44,9 @@ physio <- physio[physio$healthy == 1, ]
 #### Define response(s)
 
 #### Define response variable/sample
-yvar   <- "Mg"
-sample <- "1"
+# "pH"   "PCO2" "PO2"  "HCO3" "lac"  "glu"  "K"    "Mg"  
+yvar   <- "K"
+sample <- "2"
 resp   <- paste0(yvar, "_", sample)
 
 #### Focus on specific columns
@@ -74,38 +75,55 @@ psych::pairs.panels(physior)
 
 #### Model formulae 
 if(sample == 1){
-  form_1 <- resp ~ 
-    sex * 
+  form_1 <- resp ~
+    sex + 
+    size_len + 
+    temp_water * 
     time_from_capture_to_surface + time_from_surface_to_bs1 + 
-    size_len + temp_water + gaff
+    gaff
   form_2 <- resp ~ 
     sex + 
+    size_len + 
+    temp_water +
     time_from_capture_to_surface + time_from_surface_to_bs1 + 
-    size_len + temp_water + gaff
+    gaff
   form_3 <- resp ~ 
     sex + 
+    size_len + 
+    temp_water +
     time_from_capture_to_bs1 + 
-    size_len + temp_water + gaff
+    gaff
 } else if(sample == 2){
-  form_1 <- resp ~ 
-    sex * 
+  form_1 <- resp ~
+    sex + 
+    size_len + 
+    temp_water * 
     time_from_capture_to_surface + time_from_surface_to_bs2 + 
-    size_len + temp_water + gaff
+    gaff
   form_2 <- resp ~ 
-    sex + time_from_capture_to_surface + time_from_surface_to_bs2 + 
-    size_len + temp_water + gaff
+    sex + 
+    size_len + 
+    temp_water +
+    time_from_capture_to_surface + time_from_surface_to_bs2 + 
+    gaff
   form_3 <- resp ~ 
-    sex + time_from_capture_to_bs2 + 
-    size_len + temp_water + gaff
+    sex + 
+    size_len + 
+    temp_water +
+    time_from_capture_to_bs2 + 
+    gaff
 }
 
 #### Model fitting 
 mod_1 <- lm(form_1, data = physior)
 mod_2 <- lm(form_2, data = physior)
 mod_3 <- lm(form_3, data = physior)
+summary(mod_1)
+summary(mod_2)
+summary(mod_3)
 
 #### Model comparison
-( ranks <- AIC(mod_1, mod_2, mod_3) )
+( ranks <- AIC(mod_1, mod_2) ) # ( ranks <- AIC(mod_1, mod_2, mod_3) )
 message(round(max(ranks$AIC) - min(ranks$AIC), digits = 2))
 message(rownames(ranks)[which.min(ranks$AIC)])
 mod <- get(rownames(ranks)[which.min(ranks$AIC)])
@@ -122,12 +140,12 @@ summary(mod)
 #### Model summary (tidy)
 coef_names <- c("Intercept", 
                 "Sex (M)", 
+                "Size", 
+                "Temperature", 
                 "Time (capture → surface)", 
                 "Time (surface → sample)", 
-                "Length", 
-                "Temperature", 
                 "Gaff (Y)", 
-                "Sex (M): Time (capture → surface)")
+                "Temperature: Time (capture → surface)")
 coef_tbl <- utils.add::tidy_coef(coef = coef(summary(mod)), 
                                  coef_names = coef_names)
 tidy_write(coef_tbl, paste0("./fig/", resp, "_coef.txt"))
@@ -145,10 +163,10 @@ if(save) dev.off()
 ## Pretty x axis labels 
 # xlabs <- colnames(model.frame(mod))[2:ncol(model.frame(mod))]
 xlabs <- c("Sex", 
-           expression("Time (hook" %->% "surface) [mins]"), 
-           expression("Time (surface" %->% "BS1) [mins]"), 
            "Length [cm]", 
            expression("Temperature [" * degree * "C]"), 
+           expression("Time (hook" %->% "surface) [mins]"), 
+           expression("Time (surface" %->% "BS1) [mins]"),
            "Gaff"
            )
 if(sample == 2){
@@ -158,9 +176,21 @@ if(sample == 2){
 if(save) tiff(paste0("./fig/", resp, ".tiff"), 
               height = 5.5, width = 9, units = "in", res = 600)
 pp <- par(oma = c(2, 2, 2, 2), mar = rep(2.5, 4))
+physio_in_mod <- model.frame(mod)
+## Define point colours, shapes and sizes
+col_param <- pretty_cols_brewer(zlim = range(physio_in_mod$temp_water), 
+                                scheme = "RdYlBu"
+)
+col_param$col <- scales::alpha(col_param$col, 0.5)
+physio_in_mod$pt_col <- col_param$col[findInterval(physio_in_mod$temp_water, col_param$breaks)]
+physio_in_mod$pt_cex <- physio_in_mod$size_len/225
+physio_in_mod$pt_pch <- 21 # c(1, 4)[rates_in_mod$sex]
+pt_param <- list(col = physio_in_mod$pt_col, 
+                 cex = physio_in_mod$pt_cex, 
+                 pch = physio_in_mod$pt_pch)
 pretty_predictions_1d(model = mod, 
                       pretty_axis_args = list(control_digits = 2),
-                      add_points = list(cex = 0.5, lwd = 0.5, col = "grey20"),
+                      add_points = pt_param,
                       add_error_bars = list(add_fit = list(pch = 21, bg = "black", cex = 2)),
                       add_xlab = list(text = xlabs, line = 2.25),
                       add_ylab = list(text = ylabs[[yvar]]),
