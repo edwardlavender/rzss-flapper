cb27fe81e41ff2a830424bce5363a771e1f56420
edwardlavender <el72@st-andrews.ac.uk>
2023-07-30 10:09:06 +0200

    Account for tagged/untagged individuals at BS2

--- R/009_analyse_bloods_change.R
+++ R/009_analyse_bloods_change.R
@@ -26,6 +26,7 @@ library(prettyGraphics)
 
 #### Load data
 source(here_r("001_define_global_param.R"))
+source(here_r("002_define_helpers.R"))
 physio <- readRDS("./data/skate/physio.rds")
 
 #### Define local parameters
@@ -142,16 +143,20 @@ obs_by_param |>
     values_from = avg
   ) |>
   dplyr::mutate(
-    difference = BS2 - BS1,
-    percentage = (difference / BS1) * 100
+    diff_a = BS2 - BS1,
+    diff_n = `"BS2:" ~ S[N]` - BS1,
+    diff_y = `"BS2:" ~ S[Y]` - BS1
   ) |>
-  dplyr::arrange(percentage) |>
+  dplyr::arrange(diff_a) |>
   dplyr::select(
     Par. = resp,
-    BS1 = BS1,
-    BS2 = BS2,
-    `Absolute difference` = difference,
-    `Percentage difference` = percentage
+    BS1,
+    BS2,
+    `BS2: S[N]` = `"BS2:" ~ S[N]`, 
+   ` BS2: S[Y]` = `"BS2:" ~ S[Y]`, 
+   D =  diff_a,
+   `D: S[N]` = diff_n,
+   `D: S[Y]` = diff_y
   ) |>
   tidy_numbers(digits = 2) |>
   tidy_write(file = "./fig/blood_change_tbl.txt")
@@ -179,6 +184,11 @@ obs_by_param |>
 # ... This makes no assumptions regarding the underlying population
 # ... and can be applied to a variety of situations (Ekik & Ekik, 2012)
 
+#### Define comparison types
+types <- c("BS1 vs. BS2", 
+          "BS1 vs. BS2: S[N]", 
+          "BS1 vs. BS2: S[Y]")
+
 #### Define a dataframe of paired test statistics for each variable
 expectations <- list(
   pH = "less",
@@ -190,76 +200,98 @@ expectations <- list(
   K = "greater",
   Mg = "two.sided"
 )
-p_by_param <-
-  mapply(
-    function(resp, alt) {
-      ## Define response variables
-      # resp = "pH"; alt = "less"
-      r_1 <- paste0(resp, "_1")
-      r_2 <- paste0(resp, "_2")
 
-      ## Isolate data from individuals with observations at both BS1 and BS2
-      # (as needed for a paired test)
-      physio_for_resp <- physio[, c("pit", r_1, r_2)]
-      physio_for_resp <- physio_for_resp[complete.cases(physio_for_resp), ]
-      bs1 <- physio_for_resp[, r_1]
-      bs2 <- physio_for_resp[, r_2]
+#### Generate statistics for each comparison type 
+lapply(types, function(type) {
+  
+  p_by_param <-
+    mapply(
+      function(resp, alt) {
 
-      run <- FALSE
-      if (run) {
-        ## Run paired t test and check shapiro.test for normality
-        # Some variables have Shapiro p <= 0.05,
-        # ... implying the assumption of normality is not met
-        tt <- t.test(bs2, bs1, alternative = alt, paired = TRUE)
-        sh <- shapiro.test(bs1 - bs2)
-        message("Shapiro: ", round(sh$p.value, 2))
-
-
-        ## Run wilcox.test
-        # This compares whether the distribution of 'ranks' differs
-        # ... i.e., whether the distributions of values for BS1/BS2 differ
-        # ... but we assume equal variance
-        wi <- wilcox.test(bs2, bs1, alternative = alt, paired = TRUE, exact = FALSE)
-
-        ## Run an F test to compare the variance of the two groups
-        # For some variables, the p value is less than 0.05,
-        # ... implying a significant difference in variance.
-        ft <- var.test(x = bs1, y = bs2)
-
-        ## Run unequal variance t test using ranks (following Ruxton, 2006)
-        we <- t.test(rank(bs2), rank(bs1), alt, paired = TRUE, var.equal = FALSE)
-      }
-
-      ## Bootstrapping approach
-      bt <- wBoot::boot.paired.per(bs2, bs1, alternative = alt, null.hyp = 0, R = 1e4)
-
-      # Return outputs
-      out <- dplyr::tibble(
-        resp = resp,
-        n = nrow(physio_for_resp),
-        MD_obs = bt$Observed,
-        MD_sim = bt$Mean,
-        SE = bt$SE,
-        Bias = bt$Bias,
-        `Bias (%)` = bt$Percent.bias,
-        H0 = 0,
-        H1 = bt$Alternative,
-        `p-value` = bt$p.value
-      )
-      rownames(out) <- NULL
-      # print(out)
-      return(out)
-    },
-    names(expectations), expectations,
-    SIMPLIFY = FALSE
-  ) |>
-  dplyr::bind_rows()
-## Adjust p-values using the Bonferroni correction
-p_by_param$`p-value (BC)` <- p.adjust(p_by_param$`p-value`, method = "bonferroni")
-
-#### Write tidy table to file
-tidy_numbers(p_by_param, 3) |>
-  tidy_write("./fig/blood_change_tbl_bootstrap.txt")
+        ## Define response variables
+        # resp = "pH"; alt = "less"
+        print(paste(type, ";", resp, "----"))
+        r_1 <- paste0(resp, "_1")
+        r_2 <- paste0(resp, "_2")
+        
+        ## Isolate data from individuals with observations at both BS1 and BS2
+        # (as needed for a paired test)
+        physio_for_resp <- physio[, c("pit", "surgery", r_1, r_2)]
+        physio_for_resp <- physio_for_resp[complete.cases(physio_for_resp), ]
+        if (type == "BS1 vs. BS2: S [N]") {
+          physio_for_resp[physio_for_resp$surgery == "N", ]
+        }
+        if (type == "BS1 vs. BS2: S[N]") {
+          physio_for_resp <- physio_for_resp[physio_for_resp$surgery == "N", ]
+        } else if (type == "BS1 vs. BS2: S[Y]") {
+          physio_for_resp <- physio_for_resp[physio_for_resp$surgery == "Y", ]
+        } else {
+          if (type != "BS1 vs. BS2") {
+            stop("Unrecognised `type`!", call. = FALSE)
+          }
+        }
+        bs1 <- physio_for_resp[, r_1]
+        bs2 <- physio_for_resp[, r_2]
+        
+        run <- FALSE
+        if (run) {
+          ## Run paired t test and check shapiro.test for normality
+          # Some variables have Shapiro p <= 0.05,
+          # ... implying the assumption of normality is not met
+          tt <- t.test(bs2, bs1, alternative = alt, paired = TRUE)
+          sh <- shapiro.test(bs1 - bs2)
+          message("Shapiro: ", round(sh$p.value, 2))
+          
+          ## Run wilcox.test
+          # This compares whether the distribution of 'ranks' differs
+          # ... i.e., whether the distributions of values for BS1/BS2 differ
+          # ... but we assume equal variance
+          wi <- wilcox.test(bs2, bs1, alternative = alt, paired = TRUE, exact = FALSE)
+          
+          ## Run an F test to compare the variance of the two groups
+          # For some variables, the p value is less than 0.05,
+          # ... implying a significant difference in variance.
+          ft <- var.test(x = bs1, y = bs2)
+          
+          ## Run unequal variance t test using ranks (following Ruxton, 2006)
+          we <- t.test(rank(bs2), rank(bs1), alt, paired = TRUE, var.equal = FALSE)
+        }
+        
+        ## Bootstrapping approach
+        bt <- wBoot::boot.paired.per(bs2, bs1, alternative = alt, null.hyp = 0, R = 1e4)
+        
+        # Return outputs
+        out <- dplyr::tibble(
+          type = type,
+          resp = resp,
+          n = length(bs1),
+          MD_obs = bt$Observed,
+          MD_sim = bt$Mean,
+          SE = bt$SE,
+          Bias = bt$Bias,
+          `Bias (%)` = bt$Percent.bias,
+          H0 = 0,
+          H1 = bt$Alternative,
+          `p-value` = bt$p.value
+        )
+        rownames(out) <- NULL
+        # print(out)
+        return(out)
+        
+      },
+      names(expectations), expectations,
+      SIMPLIFY = FALSE
+    ) |>
+    dplyr::bind_rows()
+  
+  ## Adjust p-values using the Bonferroni correction
+  p_by_param$`p-value (BC)` <- p.adjust(p_by_param$`p-value`, method = "bonferroni")
+  
+  #### Write tidy table to file
+  tidy_numbers(p_by_param, 3) |>
+    tidy_write(paste0("./fig/blood_change_tbl_bootstrap_", str_strip(type), ".txt"))
+  
+}) |> invisible()
 
 
 #### End of code.
